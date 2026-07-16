5fd1f8dc9a196d9127a329ad6c84387286d1c905
amaho-arch <amaho-arch@users.noreply.github.com>
2026-07-16 14:13:26 +0300

    ci: make MorpheApp registry credentials optional

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,19 @@ rootProject.name = "morphe-patches-template"
 
 pluginManagement {
     repositories {
+        mavenLocal()
         gradlePluginPortal()
         google()
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/MorpheApp/registry")
-            credentials {
-                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GPR_USER")
-                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GPR_KEY")
+            val gprUser = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GPR_USER")
+            val gprKey = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GPR_KEY")
+            if (gprUser != null && gprKey != null) {
+                credentials {
+                    username = gprUser
+                    password = gprKey
+                }
             }
         }
         maven { url = uri("https://jitpack.io") }
